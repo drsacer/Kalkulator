4cba1d282768ac6072d1e838d6790f59fe9596cc
drsacer <drsacer@gmail.com>
2022-03-08 11:16:13 +0100

    Advanced kalkulator

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,27 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Calculator basic = new Calculator();
+        Calculator c = null ;
 
         Scanner s = new Scanner(System.in);
 
-        System.out.println("Supported operations are: " + basic.listOperations());
+        System.out.println("Welcome to great Calculator!\n" +
+                "Which calculator do you need?\n" +
+                "1 - Basic calculcator\n2 - Advanced calculator");
+
+        if (s.nextInt() == 1) {
+            c = new Calculator();
+        } else {
+            c = new AdvancedCalculator();
+        }
+
+        System.out.println("Supported operations are: " + c.listOperations());
         System.out.println("Enter expression in format <a> <operation> <b>");
 
-        basic.setA(s.nextDouble());
-        basic.setOperation(s.next());
-        basic.setB(s.nextDouble());
+        c.setA(s.nextDouble());
+        c.setOperation(s.next());
+        c.setB(s.nextDouble());
 
-        System.out.println("Result is:" + basic.calculate());
+        System.out.println("Result is:" + c.calculate());
     }
 }
